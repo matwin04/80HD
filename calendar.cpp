e242f89581c637082e510770ee746b36317c0dd0
frostie <mattwiner04@gmail.com>
2024-11-23 11:52:05 -0800

    4. Created Calendar Class

---
+++ calendar.cpp
@@ -0,0 +1,11 @@
+#include "calendar.h"
+
+calendar::calendar()
+{
+
+}
+
+calendar::~calendar()
+{
+
+}
---
+++ calendar.h
@@ -0,0 +1,16 @@
+#ifndef CALENDAR_H
+#define CALENDAR_H
+
+#pragma once
+
+class calendar
+{
+public:
+    calendar();
+    ~calendar();
+
+private:
+
+};
+
+#endif
